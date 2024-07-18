bda97f2db0bc948a02cf178268479f7ff8c96563
nkemjika <nkemjikaohanyere@gmail.com>
2024-07-18 10:18:28 +0100

    feat: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className='w-screen h-screen flex items-center'>
+      <div className='relative layout h-full flex justify-center'>
+        <div className="absolute top-0 z-[-2] h-full w-full bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+        <header>
+          nkem.
+        </header>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
