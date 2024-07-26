156def075b039848212ffccfdac5dea5689aaef1
nkemjika <nkemjikaohanyere@gmail.com>
2024-07-26 11:45:49 +0100

    refactor: seperate components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+	return <footer className="mt-7">
+		<p className="font-outfitRegular text-gray-500 ">🧌 &copy; nkemjika</p>
+	</footer>
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+
+	return <header className="relative h-[10%] flex flex-col items-center justify-center">
+		<div className="relative flex flex-row justify-center items-center gap-1 group">
+			<p className="inline-block group-hover:animate-wave text-3xl sm:text-5xl">👋🏾</p>
+			<a href="#" className="relative inline-block transition-all ease-in-out delay-1000 duration-1000 font-outfitSemiBold text-4xl sm:text-6xl bg-[length:200px_100px] bg-[-100%] before:content[''] before:block before:absolute before:left-0 before:w-0 before:h-1 before:transition-all before:bg-nav-gradient before:hover:w-full before:bottom-0">
+				nkem
+				<span id="image-spin" className="image-spin rounded-[50%]"></span>
+			</a>
+		</div>
+		<p className="mt-3 w-full  font-outfitRegular text-center text-3xl sm:text-5xl">I <span className="bg-myPink create-span hover:animate-create-text">create</span> for the web</p>
+	</header>
+}
